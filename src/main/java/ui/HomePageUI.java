91c02d0cecb9b9c6bb7d2c5a002abe223786c87c
Jessica <jessrodgu@gmail.com>
2023-12-06 19:01:28 -0400

    AddRemove Elements implementacion de test

---
+++ src/main/java/ui/HomePageUI.java
@@ -0,0 +1,10 @@
+package ui;
+
+import org.openqa.selenium.By;
+
+public class HomePageUI {
+    public static By addRemove = By.xpath("//a[.='Add/Remove Elements']");
+    public static By checkBoxes = By.xpath("//*[@id=\"content\"]/ul/li[2]/a");
+    public static By basicAuth = By.xpath("//*[@id=\"content\"]/ul/li[2]/a");
+    public static By dropDown = By.xpath("//*[@id=\"content\"]/ul/li[2]/a");
+}
