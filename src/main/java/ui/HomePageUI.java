8a65cdc69b538670b974af39e8a118262976b952
Jessica <jessrodgu@gmail.com>
2023-12-06 21:12:01 -0400

    implementacion de test para formAuthentication

--- src/main/java/ui/HomePageUI.java
+++ src/main/java/ui/HomePageUI.java
@@ -4,7 +4,8 @@ import org.openqa.selenium.By;
 
 public class HomePageUI {
     public static By addRemove = By.xpath("//a[.='Add/Remove Elements']");
-    public static By checkBoxes = By.xpath("//*[@id=\"content\"]/ul/li[2]/a");
-    public static By basicAuth = By.xpath("//*[@id=\"content\"]/ul/li[2]/a");
-    public static By dropDown = By.xpath("//*[@id=\"content\"]/ul/li[2]/a");
+    public static By checkBoxes = By.xpath("//*[@id=\"content\"]/ul/li[6]/a");
+    public static By basicAuth = By.xpath("//*[@id=\"content\"]/ul/li[3]/a");
+    public static By formAuth = By.xpath("//a[.='Form Authentication']");
+    public static By dropDown = By.xpath("//*[@id=\"content\"]/ul/li[11]/a");
 }
